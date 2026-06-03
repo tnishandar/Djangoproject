e59db6761ed3cebf34d3e393b006e2fe9ef9aaf6
tnishandar <106342842+tnishandar@users.noreply.github.com>
2026-06-03 19:03:26 +0100

    Update views.py
    missed a colon instead of semi colon

--- demo/views.py
+++ demo/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def index(request);
+def index(request):
     return render(request, 'demo_site.html')
