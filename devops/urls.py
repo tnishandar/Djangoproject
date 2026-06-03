2872578fac5d7fb323ee0bfe39a4f071b13edb96
tnishandar <106342842+tnishandar@users.noreply.github.com>
2026-06-03 16:15:31 +0100

    Update urls.py
    forgot comma

--- devops/urls.py
+++ devops/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('demo/', include('demo.urls'))
+    path('demo/', include('demo.urls')),
     path('admin/', admin.site.urls),
 ]
